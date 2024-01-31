5f7a49fd20d70dc61039b244e8357a8687ae4ac4
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-01-31 16:58:26 +0100

    ajout html, css, js + tableau assoc

---
+++ js/quotes.js
@@ -0,0 +1,7 @@
+var quotes = {
+    "Serge Karamazov":"Barrez vous **** de mimes.",
+    "Georges Abitbol":"Le train de tes injures roule sur le rail de mon indifférence.",
+    "Hubert Bonisseur de la Bath":"Ça ne prenait pas beaucoup plus de temps.",
+    "Moulinier":"L'habit ne fait pas le moine, mais il fait l'agent...Même non titularisé.",
+    "Orson Welles":"C'est du vol et du plagiat. J'aime pas trop les voleurs et..."
+};
