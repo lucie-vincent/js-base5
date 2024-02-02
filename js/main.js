734e0be9754e2bd999f9f7d4d2c345a629e44320
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-02-02 14:38:25 +0100

    ajout css + itération json : afficher auteur +titre + citation

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const container = document.querySelector("#container");
+console.log(container)
+
+for (quote of quotes) {
+    // on crée une balise html div avec la méthode createElement()
+    const citation = document.createElement("div");
+    // on ajoute une classe à la balise div avec la méthode
+    // add() de la propriété classList
+    citation.classList.add("citation")
+    
+    const content = document.createElement("p");
+    // on ajoute la propriété innerHTML pour récupérer ou définir le contenu de l'élément content créé
+    //plus haut
+    content.innerHTML = `« ${quote.content} »`;
+    // on ajoute l'élément p au DOM, on le lie à l'élément citation
+    citation.appendChild(content);
+
+    const author = document.createElement("small");
+    author.classList.add("author");
+    author.innerHTML = `${quote.author}`;
+    citation.appendChild(author);
+
+    const title = document.createElement("small");
+    title.classList.add("title");
+    title.innerHTML = ` - ${quote.title}`;
+    citation.appendChild(title);
+
+    
+    // on ajoute au container tout notre bloc citation
+    container.appendChild(citation)
+}
+
+// for (quote of quotes) {
+//     console.log(quote.author);
+// } 
+    
+// for (quote of quotes) {
+//     console.log(quote.content);
+// }
+        
+// for (quote of quotes) {
+//     console.log(`${quote.author} : "${quote.content}"` );
+// } 
